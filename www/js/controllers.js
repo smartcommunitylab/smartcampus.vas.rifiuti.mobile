0b5342746b004d18dac25d3eb7f01e96a1e8c167
Luca Miori <luca.miori@marconirovereto.it>
2014-08-01 15:14:29 +0200

    Removed unused controllers, and comments.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -100,39 +100,3 @@ angular.module('starter.controllers', [])
     }
 
 })
-
-.controller('PlaylistsCtrl', function ($scope, $rootScope) {
-    $rootScope.activePage = 'playlists';
-    $rootScope.icon = 'ion-chevron-right';
-    $scope.playlists = [
-        {
-            title: 'Reggae',
-            id: 1
-        },
-        {
-            title: 'Chill',
-            id: 2
-        },
-        {
-            title: 'Dubstep',
-            id: 3
-        },
-        {
-            title: 'Indie',
-            id: 4
-        },
-        {
-            title: 'Rap',
-            id: 5
-        },
-        {
-            title: 'Cowbell',
-            id: 6
-        }
-  ];
-})
-
-.controller('PlaylistCtrl', function ($scope, $rootScope, $stateParams) {
-    $rootScope.activePage = 'playlist';
-    $rootScope.icon = '';
-})
