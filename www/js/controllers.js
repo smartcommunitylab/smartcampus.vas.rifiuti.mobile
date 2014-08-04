b3cff1f2533f29d98a74f695691b66cc3a259897
Luca Miori <luca.miori@marconirovereto.it>
2014-08-04 17:32:21 +0200

    Finished the 'note' tab, and implemented a nav-bar.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,166 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function ($scope, $rootScope, $timeout) {
+.controller('AppCtrl', function ($scope) {})
 
-    $rootScope.activePage = 'base';
-    $rootScope.icon = '';
-    $rootScope.noteSelected = 'false';
-
-    $scope.click = function () {
-        //Da implementare!
-        if($rootScope.activePage && $rootScope.noteSelected == 'home-note')
-        {
-            $rootScope.noteSelected = 'false';
-        }
-
-    };
-
-    $scope.delete = function () {};
-
-    $scope.edit = function () {};
-})
-
-.controller('HomeCtrl', function ($scope, $rootScope, $ionicTabsDelegate, $timeout) {
+.controller('HomeCtrl', function ($scope, $ionicTabsDelegate, $ionicSideMenuDelegate, $timeout, $ionicPopup) {
 
     var delegate = $ionicTabsDelegate.$getByHandle('home-tabs');
-    var notes = [];
+    $scope.notes = [{
+        id: 0,
+        note: 'uno'
+    }, {
+        id: 1,
+        note: 'due'
+    }, {
+        id: 2,
+        note: 'tre'
+    }];
+    // nota{ID,nota}
+    $scope.selectedNotes = [];
+
+    $scope.noteSelected = false;
+    $scope.multipleNoteSelected = false;
 
     $timeout(function () {
         delegate.select(1);
     }, 50);
 
-    $scope.slideChanged = function (index) {
-        $rootScope.noteSelected = 'false';
-        switch (index) {
-        case 0:
-            $rootScope.activePage = 'home-note';
-            $rootScope.icon = 'ion-plus';
-            break;
-        case 2:
-            $rootScope.activePage = 'home-calendar';
-            $rootScope.icon = 'ion-calendar';
-            break;
-        default:
-            $rootScope.activePage = 'home-main';
-            $rootScope.icon = '';
+    $scope.addNote = function (nota) {
+        $scope.notes.push({
+            id: function () {
+                for (var i = 0; i <= $scope.notes.length; i = i + 1) {
+                    if (function (i) {
+                        for (var j = 0; j < $scope.notes.length; j = j + 1) {
+                            if ($scope.notes[j].id == i) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }) {
+                        return i;
+                    }
+                }
+            },
+            note: nota
+        });
+    };
+
+    $scope.removeNote = function (id) {
+        for (var i = 0; i < $scope.notes.length; i = i + 1) {
+            if ($scope.notes[i].id == id) {
+                $scope.notes.splice(i, 1);
+            }
+        }
+    };
+
+    $scope.noteSelect = function (nota) {
+        var p = $scope.selectedNotes.indexOf(nota);
+        if (p == -1) {
+            $scope.selectedNotes.push(nota);
+            $scope.noteSelected = true;
+            if ($scope.selectedNotes.length > 1) {
+                $scope.multipleNoteSelected = true;
+            }
+        } else {
+            $scope.selectedNotes.splice(p, 1);
+            if ($scope.selectedNotes.length <= 1) {
+                $scope.multipleNoteSelected = false;
+            }
+            if ($scope.selectedNotes.length < 1) {
+                $scope.noteSelected = false;
+            }
+        }
+    };
+
+    $scope.click = function () {
+        if ($scope.noteSelected) {
+            while ($scope.selectedNotes.length > 0) {
+                $scope.removeNote($scope.selectedNotes[0].id);
+                $scope.noteSelect($scope.selectedNotes[0]);
+            }
+        } else {
+            $scope.data = {};
+
+            var popup = $ionicPopup.show({
+                template: '<input type="text" ng-model="data.nota">',
+                title: 'Cosa vuoi ricordare?',
+                scope: $scope,
+                buttons: [
+                    {
+                        text: 'Cancel'
+                    },
+                    {
+                        text: '<b>Save</b>',
+                        type: 'button-positive',
+                        onTap: function (e) {
+                            if (!$scope.data.nota) {
+                                e.preventDefault();
+                            } else {
+                                return $scope.data.nota;
+                            }
+                        }
+                }
+            ]
+            });
+            popup.then(function (res) {
+                if (res != null && res != undefined) {
+                    $scope.addNote(res);
+                }
+            });
+        }
+    };
+
+    $scope.edit = function () {
+        $scope.data = {};
+
+        var popup = $ionicPopup.show({
+            template: '<input type="text" ng-model="data.nota">',
+            title: 'Cosa vuoi ricordare?',
+            scope: $scope,
+            buttons: [
+                {
+                    text: 'Cancel'
+                    },
+                {
+                    text: '<b>Save</b>',
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        if (!$scope.data.nota) {
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.nota;
+                        }
+                    }
+                }
+            ]
+        });
+        popup.then(function (res) {
+            if (res != null && res != undefined) {
+                $scope.notes[$scope.notes.indexOf($scope.selectedNotes[0])].note = res;
+                $scope.noteSelect($scope.selectedNotes[0]);
+            }
+        });
+    };
+
+    $scope.noteReset = function () {
+        $scope.selectedNotes = [];
+        $scope.noteSelected = false;
+        $scope.multipleNoteSelected = false;
+    };
+
+    $scope.titleText = function () {
+        if (!$scope.noteSelected) {
+            return '100% Riciclo';
+        } else {
+            return '';
+        }
+    };
+
+    $scope.leftClick = function () {
+        if (!$scope.noteSelected) {
+            $ionicSideMenuDelegate.toggleLeft();
+        } else {
+            $scope.noteReset();
         }
     };
 
@@ -56,22 +174,22 @@ angular.module('starter.controllers', [])
                 return item.indexOf(query) != -1;
             }
         }
-    }
+    };
 
     $scope.firstCol = function (item, v) {
         if (v.indexOf(item) % 3 == 0) return item;
         else return '';
-    }
+    };
 
     $scope.secondCol = function (item, v) {
         if (v.indexOf(item) % 3 == 1) return item;
         else return '';
-    }
+    };
 
     $scope.thirdCol = function (item, v) {
         if (v.indexOf(item) % 3 == 2) return item;
         else return '';
-    }
+    };
 
     $scope.oneInThree = function (v) {
         var f = [];
@@ -79,12 +197,7 @@ angular.module('starter.controllers', [])
             f[i / 3] = v[i];
         }
         return f;
-    }
+    };
 })
 
-.controller('InfoCtrl', function ($scope, $rootScope) {
-
-    $rootScope.activePage = 'info';
-    $rootScope.icon = '';
-    $rootScope.noteSelected = 'false';
-})
+.controller('InfoCtrl', function ($scope) {})
